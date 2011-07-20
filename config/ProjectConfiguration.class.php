4ca926902f52003ecbfff7a4bf03c01087624d0c
Hugues Lepesant <hugues@lepesant.com>
2011-07-20 23:02:10 +0200

    Remove sfProtoculous deprecared plugin

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -7,7 +7,7 @@ class ProjectConfiguration extends sfProjectConfiguration
 {
   public function setup()
   {
-    $this->enablePlugins('sfProtoculousPlugin');
+#    $this->enablePlugins('sfProtoculousPlugin');
 
     sfConfig::set('sf_logging_enabled', true);
 
