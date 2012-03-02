ce8324969aefe3b43e26184dee89d10ee8a03f82
Hugues Lepesant <hugues@lepesant.com>
2012-03-02 01:29:07 +0100

    CSS work (again) to use Blueprint CSS [http://www.blueprintcss.org/]

--- apps/admin/templates/layout.php
+++ apps/admin/templates/layout.php
@@ -4,10 +4,43 @@
 <?php include_metas() ?>
 <?php include_title() ?>
 <link rel="shortcut icon" href="/images/favicon.ico" />
+<?php echo stylesheet_tag('blueprint/screen.css', array('type'=>'text/css', 'media' => 'screen, projection')); ?>
+<?php echo stylesheet_tag('blueprint/print.css', array('type'=>'text/css', 'media' => 'print')); ?>
+<!--[if IE]>
+<?php echo stylesheet_tag('/css/blueprint/ie.css', array('type'=>'text/css', 'media' => 'screen, projection')) ?>
+<![endif]-->
 <?php include_stylesheets() ?>
+
 <?php include_javascripts() ?>
 </head>
 <body>
+
+<div class="container header">
+    <div class="span-24 top header-top"></div>
+    <div class="span-20 header-content">
+        <?php echo image_tag('zacacia_logo_w.png') ?>
+    </div>
+    <div class="span-4 last header-logo"></div>
+    <div class="span-24 spacer"></div>
+</div>
+
+<div class="container content">
+    <div class="span-6 border showgrid">
+        <?php if (has_slot('topnav')): ?>
+            <?php include_slot('topnav') ?>
+        <?php endif; ?>
+    </div>
+
+    <div class="span-18 last showgrid">
+        <?php echo $sf_content ?>
+    </div>
+</div>
+
+<div class="container footer">
+    <div class="span-24 bottom loud showgrid">hugues-at-lepesant-dot-co </div>
+</div>
+
+<?php /*
 <div id="wrapper">
 
 	<div id="top">
@@ -42,6 +75,7 @@
 	</div><!-- /#footer  -->
 
 </div><!-- /#wrapper -->
+*/?>
 
 </body>
 </html>
