58fe3719d120ef35bf7340e9ebafc48a52caeb81
Hugues Lepesant <hugues@lepesant.com>
2015-09-29 23:43:29 +0200

    ldaptools rocks

--- src/ZacaciaBundle/Controller/PlatformController.php
+++ src/ZacaciaBundle/Controller/PlatformController.php
@@ -3,63 +3,50 @@
 namespace ZacaciaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-#use Toyota\Component\Ldap\Core\Manager;
-#use Toyota\Component\Ldap\Platform\Native\Driver;
 
-use Dreamscapes\Ldap\Core\Ldap;
+use LdapTools\Configuration;
+use LdapTools\LdapManager;
+use LdapTools\Exception\LdapConnectionException;
 
 
 class PlatformController extends Controller
 {
     public function indexAction()
     {
-/*
-        $params = array(
-            'hostname'      => 'ldap',
-            'port'          => 389,
-            'base_dn'       => 'ou=Zacacia,ou=Applications,dc=zarafa,dc=com',
-            'bind_dn'       => 'cn=admin,dc=zarafa,dc=com',
-            'bind_password' => 'password',
-            'options'       => array(
-                LDAP_OPT_DEBUG_LEVEL => 7,
-                LDAP_OPT_PROTOCOL_VERSION => 3
-            )
-        );
+        $config = (new Configuration())->load($this->container->get('kernel')->locateResource('@ZacaciaBundle/Resources/config/zacacia.yml'));
+        $ldap = new LdapManager($config);
+
+        $query = $ldap->buildLdapQuery();
+
+        $platforms = $query->select()
+            ->from('Platform')
+            ->Where(['zacaciaStatus' => 'enable'])
+            ->getLdapQuery()
+            ->getResult();
+
+        echo "Found ".$platforms->count()." platforms.";
+        foreach ($platforms as $platform) {
+            echo "Platform: ".$platform->getCn();
+        }
+
+        $ldapObject = $ldap->createLdapObject();
 
 
-        $manager = new Manager($params, new Driver());
 
         try {
-            $manager->connect();
-        } catch ( ConnectionException $e ){
-            print "error ldap : connect";
-            exit;
+            $new_platform = $ldapObject->create('Platform')
+                ->in('ou=Platforms,ou=Zacacia,ou=Applications,dc=zarafa,dc=com')
+                ->setDn('cn=Henri,ou=Platforms,ou=Zacacia,ou=Applications,dc=zarafa,dc=com')
+                ->with([
+                    'objectClass' => ['top', 'organizationalRole', 'zacaciaPlatform'],
+                    'cn' => 'Henri',
+                    'zacaciaStatus' => 'enable'
+                ])
+                ->execute();
+        } catch (LdapConnectionException $e) {
+            echo "Failed to add user!".PHP_EOL;
+            echo $e->getMessage().PHP_EOL;
         }
-   
-        try {
-            $manager->bind('cn=admin,dc=zarafa,dc=com', 'password');
-        } catch ( ConnectionException $e ){
-            print "error ldap : bind";
-            exit;
-        }
-
-        $results = $manager->search(false, 'ou=Platforms,ou=Zacacia,ou=Applications,dc=zarafa,dc=com', '(&(objectclass=zacaciaPlatform)(objectClass=top)(objectClass=organizationalRole))');
-        print_r( $results );
-        exit;
-
-        foreach ($results as $node) {
-            echo $node->getDn();
-            foreach ($node->getAttributes() as $attribute) {
-                echo sprintf('%s => %s', $attribute->getName(), implode(',', $attribute->getValues()));
-            }
-        }
-        exit;
- */
-        $con = new Ldap('ldap://ldap');
-        $con->bind('cn=admin,dc=zarafa,dc=com', 'password');
-
-        $res = $con->search('ou=Platforms,ou=Zacacia,ou=Applications,dc=zarafa,dc=com', '(&(objectclass=zacaciaPlatform)(objectClass=top)(objectClass=organizationalRole))');
-        print_r($res->getEntries());
 
         exit;
 
---
+++ src/ZacaciaBundle/Entity/Platform.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ZacaciaBundle\Entity;
+
+class Platform
+{
+
+}
