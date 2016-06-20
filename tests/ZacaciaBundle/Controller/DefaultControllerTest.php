71547b35e2e9251811d93d4e95ab792ad820a235
Hugues Lepesant <hugues@lepesant.com>
2016-06-20 22:34:10 +0200

    back to Sf3

---
+++ tests/ZacaciaBundle/Controller/DefaultControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace Tests\ZacaciaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+    }
+}
